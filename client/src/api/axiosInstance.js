404eb370ed3a037b4e342435312c279a913b92e5
Sachin Sirvi <sirvisachin10@gmail.com>
2025-06-27 19:14:15 +1000

    Fix: remove static Authorization header from axios instance

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,34 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
-/*
-* Axios Instance
-* usecase - ensures deafult config is attached with every request
-*/
 const axiosInstance = axios.create({
-    baseURL: "https://bookmyshow-fy4x.onrender.com/",
-    headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${localStorage.getItem('token')}`
-    },
+  baseURL: "https://bookmyshow-fy4x.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json"
+  },
 });
 
-/*
-* Request Interceptors 
-* usecase - ensures latest token from localStorage is attached with every request
-* syntax - axios.interceptors.request.use(onSuccess, onError(optional));
-*/
 axiosInstance.interceptors.request.use(
-    // config is the request object that contains everything about our request: like post (url, payload, config)
-    function (config) {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    function (error) {
-        return Promise.reject(error);
+  function (config) {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
 );
 
 export default axiosInstance;
